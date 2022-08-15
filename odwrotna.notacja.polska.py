f08b0452d4eabfcb706326460413d56ae7f85f63
majajonez <majajonez@users.noreply.github.com>
2022-08-15 21:38:12 +0200

    Poprawki w operacjach na listach

--- odwrotna.notacja.polska.py
+++ odwrotna.notacja.polska.py
@@ -11,6 +11,11 @@ print(odczytane)
 
 
 
+def przeniesienie_do_kolejki():
+    kolejka.append(stos[-1])
+    del stos[-1]
+
+
 for e in odczytane:
     if e not in operator1 and e not in operator2:
         if e == "(":
@@ -18,17 +23,15 @@ for e in odczytane:
         elif e == ")":
             if len(stos) !=0:
                 while stos[-1] != "(":
-                    kolejka.append(stos[-1])
-                    stos.remove(stos[-1])
+                    przeniesienie_do_kolejki()
                 if stos[-1] == "(":
-                    stos.remove(stos[-1])
+                    del stos[-1]
         else:
             kolejka.append(e)
     elif e in operator1:
         if len(stos) != 0:
             if stos[-1] == operator1 or stos[-1] == operator2:
-                kolejka.append(stos[-1])
-                stos.remove(stos[-1])
+                przeniesienie_do_kolejki()
                 stos.append(e)
             else:
                 stos.append(e)
@@ -37,8 +40,7 @@ for e in odczytane:
     elif e in operator2:
         if len(stos) != 0:
             if stos[-1] == operator2:
-                kolejka.append(stos[-1])
-                stos.remove(stos[-1])
+                przeniesienie_do_kolejki()
                 stos.append(e)
             else:
                 stos.append(e)
@@ -47,9 +49,7 @@ for e in odczytane:
 
 
 while len(stos) != 0:
-    kolejka.append(stos[-1])
-    stos.remove(stos[-1])
-
+    przeniesienie_do_kolejki()
 
 print(stos)
 print(kolejka)
