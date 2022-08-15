c220fa3201307fa655efd514205ff58ff0429f7a
majajonez <majajonez@users.noreply.github.com>
2022-08-15 16:55:51 +0200

    Utworzenie algorytmu do przeksztacania notacji infiksowej w odwrotną notację polską

---
+++ odwrotna.notacja.polska.py
@@ -0,0 +1,62 @@
+alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+kolejka = []
+stos = []
+operator1 = ['+', '-']
+operator2 = ['*', '/']
+
+wpisane = input("wpisz działanie oddzielając znaki spacją")
+odczytane = wpisane.split(" ")
+
+
+print(odczytane)
+
+
+
+def czy_jest_w_alfabecie(E):
+    for znak in alfabet:
+        if E == znak:
+            return True
+
+
+for e in odczytane:
+    if e not in operator1 and e not in operator2:
+        if e == "(":
+            stos.append(e)
+        elif e == ")":
+            if len(stos) !=0:
+                while stos[-1] != "(":
+                    kolejka.append(stos[-1])
+                    stos.remove(stos[-1])
+                if stos[-1] == "(":
+                    stos.remove(stos[-1])
+        else:
+            kolejka.append(e)
+    elif e in operator1:
+        if len(stos) != 0:
+            if stos[-1] == operator1 or stos[-1] == operator2:
+                kolejka.append(stos[-1])
+                stos.remove(stos[-1])
+                stos.append(e)
+            else:
+                stos.append(e)
+        else:
+            stos.append(e)
+    elif e in operator2:
+        if len(stos) != 0:
+            if stos[-1] == operator2:
+                kolejka.append(stos[-1])
+                stos.remove(stos[-1])
+                stos.append(e)
+            else:
+                stos.append(e)
+        else:
+            stos.append(e)
+
+
+while len(stos) != 0:
+    kolejka.append(stos[-1])
+    stos.remove(stos[-1])
+
+
+print(stos)
+print(kolejka)
